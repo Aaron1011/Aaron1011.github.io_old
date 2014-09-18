10095e0c777c897313e2b63113eec8bf2700611f
Aaron Hill <aa1ronham@gmail.com>
2014-09-18 17:14:16 -0400

    Fix up assets

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -47,3 +47,15 @@ $(document).ready(function() {
     mainClass: 'mfp-fade'
   });
 });
+
+$(function() {
+  return $("h2, h3, h4, h5, h6").each(function(i, el) {
+    var $el, icon, id;
+    $el = $(el);
+    id = $el.attr('id');
+    icon = '<i class="fa fa-link"></i>';
+    if (id) {
+      return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+    }
+  });
+});
